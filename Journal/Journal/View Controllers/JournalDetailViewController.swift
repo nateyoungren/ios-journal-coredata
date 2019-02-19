e1720e5de1ba13b655f117bc9d2d0c49662c71da
nateyoungren <45108830+nateyoungren@users.noreply.github.com>
2019-02-19 13:06:12 -0800

    Add master files

---
+++ Journal/Journal/View Controllers/JournalDetailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  JournalDetailViewController.swift
+//  Journal
+//
+//  Created by Nathanael Youngren on 2/18/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class JournalDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        guard let entryController = entryController,
+        let title = titleTextField.text,
+        let body = detailsTextView.text else { return }
+        
+        if let entry = entry {
+            entryController.update(name: title, body: body, entry: entry)
+        } else {
+            entryController.create(name: title, body: body)
+        }
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func updateViews() {
+        guard let entry = entry else {
+            navigationItem.title = "Create Entry"
+            return
+        }
+        navigationItem.title = entry.name
+        if let title = titleTextField {
+            title.text = entry.name
+        }
+        
+        if let details = detailsTextView {
+            details.text = entry.bodyText
+        }
+    }
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var detailsTextView: UITextView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
+    var entryController: EntryController?
+}
---
+++ Journal/Journal/View Controllers/JournalTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  JournalTableViewController.swift
+//  Journal
+//
+//  Created by Nathanael Youngren on 2/18/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class JournalTableViewController: UITableViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entryController.entries.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "JournalCell", for: indexPath)
+        guard let entryCell = cell as? EntryTableViewCell else { return cell }
+        entryCell.entry = entryController.entries[indexPath.row]
+        return entryCell
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let entry = entryController.entries[indexPath.row]
+            entryController.delete(entry: entry)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowJournalDetail" {
+            guard let entryVC = segue.destination as? JournalDetailViewController,
+                let index = tableView.indexPathForSelectedRow else { return }
+            entryVC.entryController = entryController
+            entryVC.entry = entryController.entries[index.row]
+        }
+        
+        if segue.identifier == "AddJournalEntry" {
+            guard let entryVC = segue.destination as? JournalDetailViewController else { return }
+            entryVC.entryController = entryController
+        }
+    }
+    
+    let entryController = EntryController()
+
+}
